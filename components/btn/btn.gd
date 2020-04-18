3c32b714f43cb5a5598bc7ec1a32f1d89ee59a4d
floxy <florian@deeken.net>
2020-04-18 17:37:20 +0200

    Initial Commit added btn component added demo main.tscn

---
+++ components/btn/btn.gd
@@ -0,0 +1,24 @@
+extends TextureButton
+
+var toggle = false;
+export(String) var InputEventCode
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if toggle:
+		toggle_btn(InputEventCode, false)
+
+
+func _on_btn_pressed():
+	toggle_btn(InputEventCode, true)
+
+
+func toggle_btn(action, state):
+	print(InputEventCode, state)
+	var ev = InputEventAction.new()
+	ev.action = action
+	ev.pressed = state
+	Input.parse_input_event(ev)
+	toggle = state;
